d339a9e6a9894b9478373410dfb9b5e46747d5f1
Madhusudan023 <99332442+Madhusudan023@users.noreply.github.com>
2024-10-31 21:31:00 -0400

    updated

---
+++ js/app.js
@@ -0,0 +1,19 @@
+let sidebar_btn = document.getElementById("sidebarbtn");
+let flag = true; 
+sidebar_btn.addEventListener("click", function(){
+  let position = 280;
+  
+
+  if(flag === false){
+    sidebar_btn.parentElement.parentElement.style.transform=`translateX(${0}px)`;
+    sidebar_btn.style.transform=`rotate(0)`;
+    
+  }
+  else{
+    sidebar_btn.parentElement.parentElement.style.transform=`translateX(${position}px)`;
+    sidebar_btn.style.transform=`rotate(180deg)`;
+    
+  }
+
+  flag = !flag;
+})
